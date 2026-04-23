f955807fee0545824d06ab508e1609e4ebd5afad
Rossler Boquiren <rgkaboquiren@gmail.com>
2026-04-22 22:10:29 -0400

    update

--- script.js
+++ script.js
@@ -2,32 +2,50 @@
 let myLeads = [];
 let inputValue = document.getElementById('inputField');
 const savedInputs = document.getElementById('saved-inputs')
+const saveBtn = document.getElementById('save-btn');
+const deleteBtn = document.getElementById('delete-btn')
+const tabBtn = document.getElementById('tab-btn')
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderList();
+    render(myLeads);
 }
 
-const saveBtn = document.getElementById('save-btn');
-saveBtn.addEventListener('click', function() {
-    myLeads.push(inputValue.value);
-    inputValue.value = '';
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-    renderList();
-});
+tabBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
-function renderList() {
+function render(leads) {
     let listItems = '';
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target="_blank" href=${myLeads[i]}>
-                    ${myLeads[i]}
+                <a target="_blank" href=${leads[i]}>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
     savedInputs.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
+})
+
+
+saveBtn.addEventListener('click', function() {
+    myLeads.push(inputValue.value);
+    inputValue.value = '';
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+});
+
